f04e36db58d6a3890efc2a04189e5ebaae98179c
BugMonkey <15298353932@163.com>
2023-10-16 20:09:42 +0800

    first commit
    Co-Authored-By: CheerCoke <133180816+CheerCoke@users.noreply.github.com>

---
+++ invitation_card/src/App.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Author: BugMonkey 15298353932@163.com
+ * @Date: 2023-10-16 20:01:35
+ * @LastEditors: BugMonkey 15298353932@163.com
+ * @LastEditTime: 2023-10-16 20:04:56
+ * @FilePath: /wedding_invitation_card/invitation_card/src/App.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<script setup>
+
+</script>
+
+<template>
+<iframe src="https://www.greenvelope.com/card/qKLJ68g/0"></iframe>
+
+</template>
+
+<style scoped>
+body{
+  margin: 0,0;
+  background-image: url();
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+}
+</style>

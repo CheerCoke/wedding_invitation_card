fbdfd165fbd86200cdf758414d73a28cb3ce6637
BugMonkey <15298353932@163.com>
2023-11-01 15:59:52 +0800

    first commit

--- invitation_card/src/App.vue
+++ invitation_card/src/App.vue
@@ -2,20 +2,40 @@
  * @Author: BugMonkey 15298353932@163.com
  * @Date: 2023-10-16 20:01:35
  * @LastEditors: BugMonkey 15298353932@163.com
- * @LastEditTime: 2023-10-18 13:56:43
+ * @LastEditTime: 2023-11-01 15:58:36
  * @FilePath: /wedding_invitation_card/invitation_card/src/App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 
 <script setup>
 import anime from "animejs";
+import wx from "weixin-js-sdk";
+import { reactive } from "vue";
 let isPlaying = false
 
-function onClickFront(e) {
+const state = reactive({ showBtn: false})
 
+function onClickFront(e) {
+  if (isPlaying) {
+    return
+  }
   let tl = anime.timeline({
     easing: 'easeInOutSine',
-    duration: 750
+    duration: 750,
+    begin: function (anim) {
+      isPlaying = true
+    },
+    complete: function (anim) {
+      state.showBtn = true
+      // anime({
+      //   targets: '#letter',
+      //   easing: 'linear',
+      //   rotate: -30,
+      //   scale:0.6,
+      //   duration:200,
+      // })
+      // anime()
+    }
   })
   tl.add({
     targets: '#letter',
@@ -29,22 +49,49 @@ function onClickFront(e) {
   tl.add({
     targets: '.card',
     translateY: '-120%',
-    duration: 600
+    duration: 500
   })
   tl.add({
     targets: '.card',
     rotate: 90,
-    "z-index":[{ value: "5" }, { value: "10",delay:500}],
-    duration: 600,
+    "z-index": [{ value: "5" }, { value: "10", delay: 500 }],
+    duration: 500,
   })
   tl.add({
     targets: '.card',
-    width:'80vw',
-    translateY:'20%',
-    duration: 1000,
+    width: '100%',
+    translateY: '20%',
+    duration: 500,
+    left: 0,
   })
+  tl.add(
+    {
+        targets: '.card-fullscreen',
+        easing: 'linear',
+        delay:anime.stagger(200),
+        scale:[{ value: [0.75, 1], duration: 500 }],
+        opacity:1,
+        duration: 1000,
+        begin:function (anim) {
+        document.querySelector('.card-fullscreen').style.display = "block"
+      }
+      }
+  )
 
   tl.play()
+}
+
+function openMap(){
+  wx.openLocation({
+  latitude: 0, // 纬度，浮点数，范围为90 ~ -90
+  longitude: 0, // 经度，浮点数，范围为180 ~ -180。
+  name: '位置', // 位置名
+  address: '', // 地址详情说明
+  scale: 1, // 地图缩放级别,整型值,范围从1~28。默认为最大
+  infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+});
+
+
 }
 </script>
 
@@ -57,25 +104,32 @@ function onClickFront(e) {
         <div id="front"></div>
         <div id="back">
           <!-- 卡片 -->
-          <div class="card"></div>
+          <img class="card" src="./assets/card.png" />
 
           <!-- 盖住卡片的封皮 -->
           <img class="card-overlay"
-            src="https://cdn.greenvelope.com/GreenvelopeImages/envelope/cover/2030.portrait.back.png" />
+            src="./assets/hover_back.png" />
 
           <!-- 上翻部分 -->
           <img class="cover-back"
-            src="https://cdn.greenvelope.com/viewer/envelope.ashx?mode=combinedflap&coverid=2030&liningid=1022&orientation=portrait&liningcolor=6a0001&sealid=null"
+            src="./assets/cover-back.png"
             alt="trigle">
 
           <img class="cover"
-            src="https://cdn.greenvelope.com/viewer/envelope.ashx?mode=backflap&lastmodified=638324286467130000&ActivityCode=qKLJ68g&ga=0"
+            src="./assets/cover.png"
             alt="trigle">
 
 
         </div>
       </div>
+
     </div>
+    <div class="card-fullscreen">
+    <img  src="./assets/card_fullscreen.png" style="position: relative;top:0;
+  box-shadow: 100px red; width: 100%;
+  border-radius: 5px;" alt="婚礼邀请函" /></div>
+
+    <button v-if="state.showBtn" class="button" @click="openMap">前往酒店</button>
   </div>
 </template>
 
@@ -149,10 +203,8 @@ function onClickFront(e) {
 
     .card {
       width: 100%;
-      aspect-ratio: 1/0.75;
       position: absolute;
       margin: auto;
-      background-color: aqua;
       z-index: 1;
     }
 
@@ -179,6 +231,35 @@ function onClickFront(e) {
 
 
 }
+
+.card-fullscreen {
+  position: absolute;
+  width: 90%;
+  top: 30px;
+  transform: scale(0.5);
+  transition: opacity 0.3s;
+  opacity: 1;
+  display: none;
+}
+
+.button{
+  position: absolute;
+  bottom: 0;
+  width: calc(100% - 30px);
+  height: 48px;
+  margin: 15px 15px;
+  background-color: brown;
+  border-radius: 10px;
+  color: #fff;
+  border: none;
+  font-size: 14pt;
+  font-family: Roboto;
+  outline: none;
+  cursor: pointer;
+  box-shadow: 0px 4px 1px -2px rgb(0 0 0 / 20%), 0px -1px 2px 0px rgb(0 0 0 / 14%), 0px 5px 8px 1px rgb(0 0 0 / 27%);
+  transition: all 300ms ease;
+}
+
 
 
 /**
